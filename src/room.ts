337e21741bff93fd21cbf33873f907f296cfedbc
yousukeman3 <yousukeman3@gmail.com>
2025-05-19 22:11:36 +0900

    Refactor Room class constructor to extend YDurableObjects

--- src/room.ts
+++ src/room.ts
@@ -1,7 +1,10 @@
 /** A Durable Object's behavior is defined in an exported Javascript class */
 import { YDurableObjects } from 'y-durableobjects'
-import * as Y from 'yjs'
 
 export class Room extends YDurableObjects<Env> {
+
+	constructor(ctx: DurableObjectState, env: Env) {
+		super(ctx, env)
+	}
 
 }
